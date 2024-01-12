55ec547dd7685825e1ccdd698cc1a112265124ee
pbn1801 <95706722+pbn1801@users.noreply.github.com>
2024-01-12 08:36:48 +0000

    https://css-tricks.com/some-cross-browser-devtools-features-you-might-not-know/?relatedposts=1

--- main.js
+++ main.js
@@ -1,47 +1,49 @@
 
-let anh = document.querySelector('img')
+// let anh = document.querySelector('img')
 
-let arr = ['https://bonanzagolfcourse.com/wp-content/uploads/2020/04/HOTEL_10-400x400-pixels.jpg',
-            'https://bonanzagolfcourse.com/wp-content/uploads/2020/04/HOTEL_9-400x400-pixels.jpg',
-            'https://bonanzagolfcourse.com/wp-content/uploads/2020/04/HOTEL_8-400x400-pixels.jpg']
+// let arr = ['https://bonanzagolfcourse.com/wp-content/uploads/2020/04/HOTEL_10-400x400-pixels.jpg',
+//             'https://bonanzagolfcourse.com/wp-content/uploads/2020/04/HOTEL_9-400x400-pixels.jpg',
+//             'https://bonanzagolfcourse.com/wp-content/uploads/2020/04/HOTEL_8-400x400-pixels.jpg']
 
-let arrIndex = 0
+// let arrIndex = 0
 
-function Next() {
-    arrIndex++
-    arrIndex %= arr.length
-    anh.src = arr[arrIndex]
-}
+// function Next() {
+//     arrIndex++
+//     arrIndex %= arr.length
+//     anh.src = arr[arrIndex]
+// }
 
 
-function Previous() {
-    arrIndex--
-    arrIndex %= arr.length
-    anh.src = arr[arrIndex]
-}
+// function Previous() {
+//     arrIndex--
+//     arrIndex %= arr.length
+//     anh.src = arr[arrIndex]
+// }
 
-let slideshow = function() {}
-let isPlay = true
+// let slideshow = function() {}
+// let isPlay = true
 
-function Play() {
-    if (isPlay) {
-        slideshow = setInterval(Next, 1000)
-        isPlay = false
-    } else {
-        clearInterval(slideshow)
-        isPlay = true
-    } 
-}
+// function Play() {
+//     if (isPlay) {
+//         slideshow = setInterval(Next, 1000)
+//         isPlay = false
+//     } else {
+//         clearInterval(slideshow)
+//         isPlay = true
+//     } 
+// }
 
-function Stop() {
-    if (!isPlay) {
-        clearInterval(slideshow)
-        isPlay = true
-    } else {
-        slideshow = setInterval(Next, 1000)
-        isPlay = false
-    }
-}
+// function Stop() {
+//     if (!isPlay) {
+//         clearInterval(slideshow)
+//         isPlay = true
+//     } else {
+//         slideshow = setInterval(Next, 1000)
+//         isPlay = false
+//     }
+// }
+
+// Search bar
 
 
 
