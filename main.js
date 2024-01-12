845aa83d22bb5c1b1b923c86f993af4cf2c22cb5
pbn1801 <95706722+pbn1801@users.noreply.github.com>
2024-01-12 08:18:52 +0000

    Added h1 tag

---
+++ main.js
@@ -0,0 +1,47 @@
+
+let anh = document.querySelector('img')
+
+let arr = ['https://bonanzagolfcourse.com/wp-content/uploads/2020/04/HOTEL_10-400x400-pixels.jpg',
+            'https://bonanzagolfcourse.com/wp-content/uploads/2020/04/HOTEL_9-400x400-pixels.jpg',
+            'https://bonanzagolfcourse.com/wp-content/uploads/2020/04/HOTEL_8-400x400-pixels.jpg']
+
+let arrIndex = 0
+
+function Next() {
+    arrIndex++
+    arrIndex %= arr.length
+    anh.src = arr[arrIndex]
+}
+
+
+function Previous() {
+    arrIndex--
+    arrIndex %= arr.length
+    anh.src = arr[arrIndex]
+}
+
+let slideshow = function() {}
+let isPlay = true
+
+function Play() {
+    if (isPlay) {
+        slideshow = setInterval(Next, 1000)
+        isPlay = false
+    } else {
+        clearInterval(slideshow)
+        isPlay = true
+    } 
+}
+
+function Stop() {
+    if (!isPlay) {
+        clearInterval(slideshow)
+        isPlay = true
+    } else {
+        slideshow = setInterval(Next, 1000)
+        isPlay = false
+    }
+}
+
+
+
